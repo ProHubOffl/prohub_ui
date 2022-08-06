b7f22da1aa67ef1c980564375e7d7d5b76ba5293
Sivanujan1998 <ssivanujan1998@gmail.com>
2022-08-06 23:46:20 +0530

    Team detail Component Created & data have fetched

--- src/components/Pages/ProjectDashboard.js
+++ src/components/Pages/ProjectDashboard.js
@@ -6,14 +6,54 @@ import ProjectUserService from '../../service/user/ProjectUserService';
 import "../../Style/ProjectDashboard.css"
 import ProgressBar from 'react-bootstrap/ProgressBar';
 import BurnDownChart from './BurnDownChart';
+import TeamTable from './TeamTable';
+import {Link} from "react-router-dom";
+import DocumentService from '../../service/document/DocumentService';
+import Chart from "../../assets/chart.svg";
+import Team from "../../assets/team.svg";
+
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+
+const style1 = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: "85%",
+  bgcolor: 'background.paper',
+  border: '1px solid rgba(54, 113, 136, 0.568)',
+  borderRadius: '20px',
+  boxShadow: 10,
+};
+const style2 = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: "85%",
+    bgcolor: 'background.paper',
+    border: '1px solid rgba(54, 113, 136, 0.568)',
+    borderRadius: '20px',
+    boxShadow: 10,
+  };
 
 const ProjectDashboard = () => {
     const selectedproject = AuthService.getCurrentProject()
-    const[users, setUsers] = useState([]);
+    const[documentcount, setDocumentCount] = useState(0);
+    const[announcecount, setAnnounceCount] = useState(0);
     const[project, setProject] = useState({})
     const[userError, setUserError] = useState('');
     const[backlogs, setBacklogs] = useState([]);
     const[backlogError, setBacklogError] = useState('');
+    const [open1, setOpen1] = React.useState(false);
+    const [open2, setOpen2] = React.useState(false);
+    const handleOpen1 = () => setOpen1(true);
+    const handleClose1 = () => setOpen1(false);
+    const handleOpen2 = () => setOpen2(true);
+    const handleClose2 = () => setOpen2(false);
 
     const CalculateDays = (date1) => {
         var end = new Date(date1);
@@ -153,6 +193,14 @@ const ProjectDashboard = () => {
           setBacklogError('Unable to fetch backlog list at the moment')
         });
 
+        DocumentService.getDocumentCountByProject(selectedproject.projectName)
+        .then(response => {
+            setDocumentCount(response.data)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+
     },[])
     //new Date()
 
@@ -208,7 +256,54 @@ const ProjectDashboard = () => {
                     <span className="data">{CalculateStoryProgress(backlogs)}</span>
                 </div>
             </div>
-            <BurnDownChart project={project} backlogs={backlogs} days={calculateSprintCapacity(project.startDate,project.endDate,project.totalSprints)}/>
+
+            <div className="row numdata">
+                <div className="col-6 col-md-3 cell1">
+                    <span className="boxtitle">Total Documents</span><br/>
+                    <span className="boxdata">{documentcount}</span><br/>
+                    <span className="dataunit"><Link to="/Document" className='linktext'>Click to View</Link></span>
+                </div>
+                <div className="col-6 col-md-3 cell2">
+                    <span className="boxtitle">Total Announcements</span><br/>
+                    <span className="boxdata">{announcecount}</span><br/>
+                    <span className="dataunit"><Link to="/Announcement" className='linktext'>Click to View</Link></span>
+                </div>
+                <div className="col-6 col-md-3 cell3">
+                    <span className="boxtitle">Burn Down Chart</span><br/>
+                    <span className="boxdata" onClick={handleOpen1}><img className='dashimg1' src={Chart} alt="Chart" /></span><br/>
+                    <span className="dataunit viewmodal" onClick={handleOpen1}>Click to View</span>
+                </div>
+                <div className="col-6 col-md-3 cell4">
+                    <span className="boxtitle">Team Details</span><br/>
+                    <span className="boxdata" onClick={handleOpen2}><img className='dashimg2' src={Team} alt="Team" /></span><br/>
+                    <span className="dataunit viewmodal" onClick={handleOpen2}>Click to View</span>
+                </div>
+            </div>
+
+            <Modal
+                open={open1}
+                onClose={handleClose1}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={style1}>
+                    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                        <BurnDownChart project={project} backlogs={backlogs} days={calculateSprintCapacity(project.startDate,project.endDate,project.totalSprints)}/>
+                    </Typography>
+                </Box>
+            </Modal>
+            <Modal
+                open={open2}
+                onClose={handleClose2}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={style2}>
+                    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                    <TeamTable project={project} backlogs={backlogs}/>
+                    </Typography>
+                </Box>
+            </Modal>
         </div>
     );
 };
