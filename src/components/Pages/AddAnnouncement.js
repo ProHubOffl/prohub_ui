62a55ea831419080e07b711b6a0db81b1cf0a216
Thiluxan <thiluxan@hotmail.co.uk>
2022-08-10 12:23:26 +0530

    Announcement UI Operations Added

--- src/components/Pages/AddAnnouncement.js
+++ src/components/Pages/AddAnnouncement.js
@@ -1,7 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
+import AnnouncementService from '../../service/announcement/AnnouncementService';
+import AuthService from '../../service/authentication/AuthService';
 import "../../Style/Project.css";
+import { toast, ToastContainer } from 'react-toastify';
 
 function AddAnnouncement() {
+    const currentUser = AuthService.getCurrentUser();
+    const currentProject = AuthService.getCurrentProject().projectName;
+
+    const[title,setTitle] = useState('')
+    const[description,setDescription] = useState('')
+
+    const handleAddAnnouncement = (e) => {
+        e.preventDefault();
+        const newAnnouncement = {
+            projectName:currentProject,
+            title,
+            description,
+            author:currentUser.email,
+            createdDate:new Date().toUTCString()
+        }
+        AnnouncementService.addAnnouncement(newAnnouncement)
+        .then(response => {
+            toast.success('Announcement Added Successfully', {
+                position: "top-center",
+                autoClose: 2500,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+            });
+            window.location.replace("/Announcement")
+            setTitle('')
+            setDescription('')
+        })
+        .catch(err => {
+            toast.error('Unable to proceed your request', {
+                position: "top-center",
+                autoClose: 2500,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+            });
+        })
+    }
     return (
         <div className="project-form">
             <div className="modal-dialog modal-dialog">
@@ -15,24 +60,35 @@ function AddAnnouncement() {
                             <div className="row">
                                 <div className="col-md-12">
                                     <label for="mail" className="form-label">Title *</label>
-                                    <input type="text" className="form-control" id="mail" placeholder="Enter Announcement Title" required/>
+                                    <input type="text" className="form-control" id="mail" placeholder="Enter Announcement Title" value={title} onChange={(e) => setTitle(e.target.value)} required/>
                                 </div>
                             </div>
                             
                             <div className="row">
                                 <label for="description" className="form-label">Description *</label>
                                 <div class="input-group mb-3">
-                                    <textarea rows="5" className="form-control border-secondary" id="description" placeholder="Enter the Description" required></textarea>
+                                    <textarea rows="5" className="form-control border-secondary" id="description" placeholder="Enter the Description" value={description} onChange={(e) => setDescription(e.target.value)} required></textarea>
                                 </div>
                             </div>
                         </div>
                         <div className="modal-footer">
                             <button type="button" className="btn btn-secondary fw-bolder" id="btn-project-close" data-bs-dismiss="modal">Cancel</button>
-                            <button type="submit" className="btn btn-primary fw-bolder" id="btn-project-create">Add</button>
+                            <button type="submit" className="btn btn-primary fw-bolder" id="btn-project-create" onClick={handleAddAnnouncement}>Add</button>
                         </div>
                     </div>
                 </form>
             </div>
+            <ToastContainer
+                position="top-center"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+            />
         </div>
     );
 }
--- src/components/Pages/UpdateAnnouncement.js
+++ src/components/Pages/UpdateAnnouncement.js
@@ -1,7 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+import AnnouncementService from '../../service/announcement/AnnouncementService';
 import "../../Style/Project.css";
+import { toast, ToastContainer } from 'react-toastify';
 
-function UpdateAnnouncement() {
+function UpdateAnnouncement(props) {
+    const[newTitle,setNewTitle] = useState('')
+    const[newDescription,setNewDescription] = useState('')
+
+    const handleUpdateAnnouncement = (e) => {
+        e.preventDefault();
+        const updatedAnnouncement = {
+            announcementId:props.announcement.announcementId,
+            projectName:props.announcement.projectName,
+            title:newTitle === '' ? props.announcement.title : newTitle,
+            description:newDescription === '' ? props.announcement.description : newDescription,
+            author:props.announcement.author,
+            createdDate:props.announcement.createdDate,
+            updatedDate:new Date().toUTCString()
+        }
+        AnnouncementService.updateAnnouncement(updatedAnnouncement,props.announcement.announcementId)
+        .then(response => {
+            toast.success('Announcement Updated Successfully', {
+                position: "top-center",
+                autoClose: 2500,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+            });
+            window.location.replace("/Announcement")
+            setNewTitle('')
+            setNewDescription('')
+        })
+        .catch(err => {
+            console.log(err)
+            toast.error('Unable to proceed your request', {
+                position: "top-center",
+                autoClose: 2500,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+            });
+        })
+    }
     return (
         <div className="project-form">
             <div className="modal-dialog modal-dialog">
@@ -15,24 +59,35 @@ function UpdateAnnouncement() {
                             <div className="row">
                                 <div className="col-md-12">
                                     <label for="mail" className="form-label">Title *</label>
-                                    <input type="text" className="form-control" id="mail" placeholder="Enter Announcement Title" required/>
+                                    <input type="text" className="form-control" id="mail" placeholder="Enter Announcement Title" defaultValue={props.announcement.title} onChange={(e) => setNewTitle(e.target.value)} required/>
                                 </div>
                             </div>
                             
                             <div className="row">
                                 <label for="description" className="form-label">Description *</label>
                                 <div class="input-group mb-3">
-                                    <textarea rows="5" className="form-control border-secondary" id="description" placeholder="Enter the Description" required></textarea>
+                                    <textarea rows="5" className="form-control border-secondary" id="description" placeholder="Enter the Description" defaultValue={props.announcement.description} onChange={(e) => setNewDescription(e.target.value)} required></textarea>
                                 </div>
                             </div>
                         </div>
                         <div className="modal-footer">
                             <button type="button" className="btn btn-secondary fw-bolder" id="btn-project-close" data-bs-dismiss="modal">Cancel</button>
-                            <button type="submit" className="btn btn-primary fw-bolder" id="btn-project-create">Update</button>
+                            <button type="submit" className="btn btn-primary fw-bolder" id="btn-project-create" onClick={handleUpdateAnnouncement}>Update</button>
                         </div>
                     </div>
                 </form>
             </div>
+            <ToastContainer
+                position="top-center"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+            />
         </div>
     );
 }
