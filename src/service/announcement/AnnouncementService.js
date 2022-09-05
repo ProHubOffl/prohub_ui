d46004aa2c9293521eb26cacaf00841d6e2502ed
Thiluxan <thiluxan@hotmail.co.uk>
2022-09-05 11:44:06 +0530

    Added Announcement Service

---
+++ src/service/announcement/AnnouncementService.js
@@ -0,0 +1,69 @@
+import { API_URL } from "../../data/ApiUrl";
+import axios from "axios";
+import AuthService from "../authentication/AuthService";
+
+const user = AuthService.getCurrentUser();
+
+const getAnnouncementsByProject = (projectName) => {
+    const URL = API_URL + projectName + "/announcement";
+    return axios.get(URL, {
+        headers: {
+            "Content-Type": "application/json",
+            'Authorization':'Bearer ' + user.jwtToken
+        }
+    });
+}
+
+const getAnnouncementByAnnouncementId = (announcementId) => {
+    const URL = API_URL + "announcement/" + announcementId;
+    return axios.get(URL, {
+        headers: {
+            "Content-Type": "application/json",
+            'Authorization':'Bearer ' + user.jwtToken
+        }
+    });
+}
+
+const addAnnouncement = (newAnnouncement) => {
+    const URL = API_URL + "announcement"
+    return axios.post(URL, newAnnouncement, {
+        headers: {
+            "Content-Type": "application/json",
+            'Authorization':'Bearer ' + user.jwtToken
+        }
+    })
+}
+
+const updateAnnouncement = (updatedAnnouncement, announcementId) => {
+    const URL = API_URL + "announcement/" + announcementId;
+    return axios.put(URL, updatedAnnouncement, {
+        headers: {
+            "Content-Type": "application/json",
+            'Authorization':'Bearer ' + user.jwtToken
+        }
+    })
+}
+
+const deleteAnnouncement = (announcementId) => {
+    const URL = API_URL + "announcement/" + announcementId;
+    return axios.delete(URL,{
+        headers: {
+            "Content-Type": "application/json",
+            'Authorization':'Bearer ' + user.jwtToken
+        }
+    })
+}
+
+const getAnnouncementCountByProject = (projectName) => {
+    const URL = API_URL + "announcement/count/" + projectName;
+    return axios.get(URL, {
+        headers: {
+            "Content-Type": "application/json",
+            'Authorization':'Bearer ' + user.jwtToken
+        }
+    });
+}
+
+export default {
+    getAnnouncementsByProject, getAnnouncementByAnnouncementId, addAnnouncement, updateAnnouncement, deleteAnnouncement, getAnnouncementCountByProject
+}
